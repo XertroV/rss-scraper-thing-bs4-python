8e570290b8c159534a67228da806dc7bf7d51468
Max Kaye <m@xk.io>
2020-09-03 14:00:03 +1000

    update fpm Thu 03 Sep 2020 14:00:03 AEST

--- run-fpm.sh
+++ run-fpm.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-cd $HOME/src/rss-scraper-thing-bs4-python \
-&& date > ./cron-01-time.log \
-&& python3 fpm.py > ./cron-02-rss.log \
+cd $HOME/src/rss-scraper-thing-bs4-python
+(date | tee cron-01-time.log \
+&& python3 -m pip install --user -r requirements.txt \
+&& python3 fpm.py | tee cron-02-rss.log \
 && git add fpm.xml \
 && git commit -m "update fpm $(date)" \
 && git push
+) 2>&1 | tee ./cron-00-full.log
